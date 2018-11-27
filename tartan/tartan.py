9797d3e7126bce6f9e71a19849e3ce6c64e9ea0e
Takudzwa Makoni <takudzwamilli@gmail.com>
2018-11-27 01:24:06 +0000

    makoni tartan

--- tartan/tartan.py
+++ tartan/tartan.py
@@ -1,15 +1,26 @@
 import PIL.Image as I
 
+#  K=#101010#;W=#E0E0E0#;Y=#FFD700#;B=#3474FC#;DR=#880000#
+#  DR/2W8K3Y4W34B2W2K4W16Y4W30B2K13
+filename = 'makoni.txt'
+
+with open(filename) as f:
+    lines = f.readlines()
+    count = 0
+    for i in lines:
+        numbers = i.split()[1]
+        count += int(numbers)
+
 unit_length = 2
 canvas_size = 1000
 canvas = I.new('RGB', (canvas_size, canvas_size), 'white')
-
-pallet = {'HG':'#285800','K':'#101010','R':'#C80000','W':'#E0E0E0','Y':'#E8C000'}
-
+pallet = {'HG':'#285800','K':'#101010','R':'#C80000','W':'#E0E0E0','Y':'#E8C000', 'LN':'#C0C0C0','TK':'#8C7038','RB':'#0C585C','DR':'#880000','GO':'#FFD700','MB':'#3474FC'}
 offset = 0
-filename = 'testfile.txt'
+
 with open(filename) as f:
-    for i in f.readlines():
+    halfsett = f.readlines()
+    sett = halfsett + halfsett[::-1]
+    for i in sett:
         pixel_data = [j for j in i.split()]
         pixel_colour, pixel_size = pallet[pixel_data[0]], int(pixel_data[1])
         pixel = I.new('RGB', (unit_length * pixel_size, unit_length * pixel_size), pixel_colour)
@@ -19,7 +30,7 @@ with open(filename) as f:
         for j in range(pixel_size):
             x_offset_diverge = offset
             y_offset_diverge = offset
-            for i in range(200):
+            for i in range(700):
                 canvas.paste( m_pixel, (x_offset_diverge + j, offset2 ) )
                 canvas.paste( m_pixel, (offset2,y_offset_diverge + j) )
                 y_offset_diverge += unit_length #2*(( (-1)**j) +1 ) * unit_length  #  was 2*
@@ -30,7 +41,7 @@ with open(filename) as f:
         for j in range(pixel_size):
             x_offset_diverge = offset
             y_offset_diverge = offset
-            for i in range(100):
+            for i in range(700):
                 canvas.paste( m_pixel, (x_offset_diverge + j, offset2 ) )
                 canvas.paste( m_pixel, (offset2,y_offset_diverge + j) )
                 y_offset_diverge +=  -2 * unit_length #2*(( (-1)**j) +1 ) * unit_length  #  was 2*
